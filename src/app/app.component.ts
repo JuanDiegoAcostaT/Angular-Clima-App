d0c86c01812797c3a0c5f0b29ef4f728a647edd9
Juan Diego Acosta <juandiegoat01@gmail.com>
2020-08-03 17:50:39 -0500

    Finishing App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,41 @@ import { WeatherService } from './services/weather.service';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  constructor(private weatherService: WeatherService) {}
+
+  location = { cityName: 'London', countryCode: 'uk' };
+  weather = undefined;
+
+  constructor(private weatherService: WeatherService) { }
 
   ngOnInit() {
-    this.weatherService.getWeather('cali', 'co')
-    .subscribe(
-      res => console.log(res),
-      err => console.log(err)
-    )
+    this.getWeather(this.location.cityName, this.location.countryCode);
   }
+
+  getWeather(cityName: string, countryCode: string) {
+    this.weatherService
+      .getWeather(cityName, countryCode)
+      .subscribe(
+        res => {
+          console.log(res);
+          this.weather = res;
+        },
+        err => {
+          console.log(err);
+        }
+      );
+  }
+
+  submitLocation(cityName: HTMLInputElement, countryCode: HTMLInputElement) {
+    if (cityName.value && countryCode.value) {
+      this.getWeather(cityName.value, countryCode.value);
+
+      cityName.value = '';
+      countryCode.value = '';
+    } else {
+      alert('Please. Insert some values');
+    }
+    cityName.focus();
+    return false;
+  }
+
 }
